6246e605bc5f5ea677269ea3b6a178616e719c5e
AlphaIsYour <alphrenoorz@gmail.com>
2025-10-07 21:18:37 +0700

    v0.0.2

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("Halooo");
+console.log("Haloo aku console");
+document.getElementById("judul").innerHTML = "Ini Navbar - Dari Script JS!";
+document.querySelector(".coba h3").style.color = "blue";
